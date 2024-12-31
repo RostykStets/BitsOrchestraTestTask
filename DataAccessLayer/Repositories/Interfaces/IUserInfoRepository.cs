ca688e1fe1e0c5ce26bf3cc3e4db19a5b1e0b435
Rostyk Stets <92711703+RostykStets@users.noreply.github.com>
2024-12-31 13:34:44 +0200

    Add and implement UserInfoRepository to retrieve, save and modify data in DataAccessLayer

---
+++ DataAccessLayer/Repositories/Interfaces/IUserInfoRepository.cs
@@ -0,0 +1,14 @@
+﻿using DataAccessLayer.Entities;
+
+
+namespace DataAccessLayer.Repositories.Interfaces
+{
+    public interface IUserInfoRepository
+    {
+        Task<IEnumerable<UserInfo>> GetUserInfos();
+        Task InsertUserInfo(UserInfo userInfo);
+        Task UpdateUserInfo(UserInfo userInfo);
+        Task DeleteUserInfo(int userInfoId);
+        Task Save();
+    }
+}
