6418f4e986fa5556d568713333e439f32d3e60ef
Rostyk Stets <92711703+RostykStets@users.noreply.github.com>
2024-12-31 14:11:04 +0200

    Create UserInfoDto to transfer data from DataAccessLayer to PresentationLayer (BitsOrchestraTestTask) and in reverse, add and implement UserInfoService to pass data from PL to DAL to CRUD data and in reverse. Add repository and service interfaces and their implementations in Program.cs for Dependency Injection.

---
+++ BusinessLogicLayer/Services/Implementations/UserInfoService.cs
@@ -0,0 +1,39 @@
+﻿using BusinessLogicLayer.DTOs;
+using BusinessLogicLayer.Services.Interfaces;
+using DataAccessLayer.Repositories.Interfaces;
+
+namespace BusinessLogicLayer.Services.Implementations
+{
+    public class UserInfoService : IUserInfoService
+    {
+        private readonly IUserInfoRepository _userInfoRepository;
+
+        public UserInfoService(IUserInfoRepository userInfoRepository)
+        {
+            _userInfoRepository = userInfoRepository;
+        }
+
+        public async Task DeleteUserInfo(int userInfoId)
+        {
+            await _userInfoRepository.DeleteUserInfo(userInfoId);
+        }
+
+        public async Task<List<UserInfoDto>> GetUserInfos()
+        {
+            var userInfos = await _userInfoRepository.GetUserInfos();
+            var userInfosDto = from userInfo in userInfos
+                               select new UserInfoDto(userInfo);
+            return userInfosDto.ToList();
+        }
+
+        public async Task InsertUserInfo(UserInfoDto userInfoDto)
+        {
+            await _userInfoRepository.InsertUserInfo(userInfoDto.toEntity());
+        }
+
+        public async Task UpdateUserInfo(UserInfoDto userInfoDto)
+        {
+            await _userInfoRepository.UpdateUserInfo(userInfoDto.toEntity());
+        }
+    }
+}
