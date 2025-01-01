dc7bc3e0db5d45045cdf8cc86323880dc1cd664a
Rostyk Stets <92711703+RostykStets@users.noreply.github.com>
2025-01-01 21:57:18 +0200

    Implement saving and deleting data and uploading csv file in UserInfoController, implement filtering and sorting data on client-side

--- BistOrchestraTestTask/Controllers/UserInfoController.cs
+++ BistOrchestraTestTask/Controllers/UserInfoController.cs
@@ -24,17 +24,62 @@ namespace BistOrchestraTestTask.Controllers
             return View(_userInfoList);
         }
 
+        [HttpPost]
+        public async Task<IActionResult> UploadCSV(IFormFile csv)
+        {
+            if (csv != null && csv.Length > 0)
+            {
+                using (var reader = new StreamReader(csv.OpenReadStream()))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        UserInfo newUserInfo = GetDataFromCSV(line);
+                        await _userInfoService.InsertUserInfo(newUserInfo.ToDto());
+                    }
+                }
+            }
+
+            return RedirectToAction("Index", "UserInfo");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save(int id, string name, DateOnly dateOfBirth, bool married, string phone, decimal salary)
+        {
+            var userInfoToUpdate = new UserInfo
+            {
+                Id = id,
+                Name = name,
+                DateOfBirth = dateOfBirth,
+                Married = married,
+                Phone = phone,
+                Salary = salary
+            }.ToDto();
+
+            await _userInfoService.UpdateUserInfo(userInfoToUpdate);
+
+            return RedirectToAction("Index", "UserInfo");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int userInfoId)
+        {
+            if (userInfoId > 0)
+                await _userInfoService.DeleteUserInfo(userInfoId);
+
+            return RedirectToAction("Index", "UserInfo");
+        }
+
         private UserInfo GetDataFromCSV(string line)
         {
             var data = line.Split(',');
             var userInfo = new UserInfo
             {
-                Id = Convert.ToInt32(data[0]),
-                Name = data[1].ToString(),
-                DateOfBirth = DateOnly.Parse(data[2]),
-                Married = Convert.ToBoolean(data[3]),
-                Phone = data[4].ToString(),
-                Salary = Convert.ToDecimal(data[5])
+                Name = data[0].ToString(),
+                DateOfBirth = DateOnly.Parse(data[1]),
+                Married = Convert.ToBoolean(data[2]),
+                Phone = data[3].ToString(),
+                Salary = Convert.ToDecimal(data[4])
             };
             return userInfo;
         }
