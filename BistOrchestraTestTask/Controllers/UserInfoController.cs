df7b1a15fa58c741cf219c42df35a7354e580cfa
Rostyk Stets <92711703+RostykStets@users.noreply.github.com>
2025-01-01 14:49:10 +0200

    Create UserInfo model in PresentationLayer (BitsOrchestraTestTask), create UserInfoController with basic Index() method and implement GetDataFromCSV() method to parse data from csv to UserInfo model

---
+++ BistOrchestraTestTask/Controllers/UserInfoController.cs
@@ -0,0 +1,42 @@
+﻿using BistOrchestraTestTask.Models;
+using BusinessLogicLayer.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BistOrchestraTestTask.Controllers
+{
+    public class UserInfoController : Controller
+    {
+        private readonly IUserInfoService _userInfoService;
+        private readonly List<UserInfo> _userInfoList;
+
+        public UserInfoController(IUserInfoService userInfoService)
+        {
+            _userInfoService = userInfoService;
+            _userInfoList = new List<UserInfo>();
+        }
+        public async Task<IActionResult> Index()
+        {
+            var userInfoDtoList = await _userInfoService.GetUserInfos();
+
+            foreach (var userInfoDto in userInfoDtoList)
+                _userInfoList.Add(new UserInfo(userInfoDto));
+
+            return View(_userInfoList);
+        }
+
+        private UserInfo GetDataFromCSV(string line)
+        {
+            var data = line.Split(',');
+            var userInfo = new UserInfo
+            {
+                Id = Convert.ToInt32(data[0]),
+                Name = data[1].ToString(),
+                DateOfBirth = DateOnly.Parse(data[2]),
+                Married = Convert.ToBoolean(data[3]),
+                Phone = data[4].ToString(),
+                Salary = Convert.ToDecimal(data[5])
+            };
+            return userInfo;
+        }
+    }
+}
---
+++ BistOrchestraTestTask/Models/UserInfo.cs
@@ -0,0 +1,49 @@
+﻿using BusinessLogicLayer.DTOs;
+using DataAccessLayer.Entities;
+
+namespace BistOrchestraTestTask.Models
+{
+    public class UserInfo
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateOnly DateOfBirth { get; set; }
+        public bool Married { get; set; }
+        public string Phone { get; set; }
+        public decimal Salary { get; set; }
+
+        public UserInfo()
+        {
+            Id = 0;
+            Name = "";
+            DateOfBirth = new DateOnly();
+            Married = false;
+            Phone = "";
+            Salary = 0;
+        }
+
+        public UserInfo(UserInfoDto userInfoDto)
+        {
+            Id = userInfoDto.Id;
+            Name = userInfoDto.Name;
+            DateOfBirth = userInfoDto.DateOfBirth;
+            Married = userInfoDto.Married;
+            Phone = userInfoDto.Phone;
+            Salary = userInfoDto.Salary;
+        }
+
+        public UserInfoDto ToDto()
+        {
+            var userInfoDto = new UserInfoDto()
+            {
+                Id = this.Id,
+                Name = this.Name,
+                DateOfBirth = this.DateOfBirth,
+                Married = this.Married,
+                Phone = this.Phone,
+                Salary = this.Salary
+            };
+            return userInfoDto;
+        }
+    }
+}
